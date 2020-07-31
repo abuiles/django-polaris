8dad8d7385dcf93b68d3f77235f33368744e1db3
Adolfo Builes <builes.adolfo@gmail.com>
2020-07-31 15:16:04 -0500

    Add runsslserver

--- example/server/settings.py
+++ example/server/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "sslserver",
     "rest_framework",
     "corsheaders",
     "server",
